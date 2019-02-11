5987ffb051dee9d4cca72857070c3e47a4b2124f
Ghasem Kiani <ghasemkiani@gmail.com>
2019-02-11 23:47:50 +0330

    v1.0.0

---
+++ fa.js
@@ -0,0 +1,15 @@
+//	@ghasemkiani/ling/fa
+
+const {Base} = require("@ghasemkiani/commonbase/base");
+const {cutil} = require("@ghasemkiani/commonbase/cutil");
+
+class Fa extends Base {
+	//
+}
+cutil.extend(Fa.prototype, {
+	//
+});
+
+const fa = new Fa();
+
+module.exports = {Fa, fa};
---
+++ index.js
@@ -0,0 +1,5 @@
+//	@ghasemkiani/ling
+
+const {Fa, fa} = require("@ghasemkiani/ling/fa");
+
+module.exports = {Fa, fa};
